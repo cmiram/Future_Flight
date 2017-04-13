c7222711fb04c239050f1132cbbb543d612be7c5
Chris Miramontes <nyyankeeschris@gmail.com>
2017-04-13 18:13:40 -0400

    initial commit to setup project. Should have all npm modules as dependencies and is set to run on port 3010 locally.

---
+++ public/config.js
@@ -0,0 +1,9 @@
+(function () {
+    angular
+        .module("Future_Flight")
+        .config(Config);
+
+    function Config($routeProvider) {
+        //for routing to pages
+    }
+})();
---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
+
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3010;
+var passport = require('passport');
+var session = require('express-session');
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+var connectionString;
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+else {
+    connectionString = 'mongodb://127.0.0.1:27017/Future_Flight';
+}
+mongoose.connect(connectionString);
+
+require('./project/app')(app);
+app.listen(port, ipaddress);
